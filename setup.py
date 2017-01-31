95b23840dfd13cc92dd035249781dc1014533322
Никита Белоногов <belonogovna@sgu.ru>
2017-01-31 10:54:14 +0400

    Init structure
    Init basic structure of project

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='project-411',
+
+    version='1.2.0',
+
+    description='Simple chatbot',
+    long_description=long_description,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=find_packages(exclude=['docs', 'tests']),
+)
