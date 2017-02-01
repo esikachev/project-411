128b5407d90c1e2bca51cc00eff923c8335ef5fe
Никита Белоногов <belonogovna@sgu.ru>
2017-02-01 19:52:17 +0400

    implement simle chat (#20)
    * csscss
    
    * messaging
    
    * mv socketio init to jsjs
    
    * change chat sides
    
    * change chat sides
    
    * add bot stub

--- app/events.py
+++ app/events.py
@@ -1,6 +1,10 @@
+from flask_socketio import emit
+
 from app import socketio
+from bot import response
 
 
 @socketio.on('message')
 def handle_message(message):
+    emit('message', response())
     print('received message: ' + str(message))
--- app/static/js.js
+++ app/static/js.js
@@ -1,3 +1,22 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function () {
+    emit('message', 'I\'m connected!');
+});
+socket.on('message', function(data){
+    print_message('bot_message', data);
+});
+
 function emit(event, message) {
     socket.emit(event, message);
+}
+
+function send_message(message) {
+    emit('message', message)
+    print_message('client_message', message)
+    // TODO: clear message input box
+}
+
+function print_message(owner, message) {
+    document.getElementById("chat_box").innerHTML +=
+        "<div><div class='message " + owner + "'>" + message + "</div></div>";
 }
