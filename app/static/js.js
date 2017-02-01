3f63e8455805c9927d5b43be49835f4cb2661c5a
nbelonogov <belonogovna@sgu.ru>
2017-02-01 17:43:30 +0400

    messaging

--- app/static/js.js
+++ app/static/js.js
@@ -2,7 +2,9 @@ function emit(event, message) {
     socket.emit(event, message);
 }
 
-function send_message(object) {
-    emit('message', object.textContent)
+function send_message(message) {
+    emit('message', message)
+    document.getElementById("chat_box").innerHTML += "<div><div class='message client_message'>" + message +"</div></div>";
+    // TODO: add this message to
     // TODO: clear message input box
 }
