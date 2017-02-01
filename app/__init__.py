de60dc91b3e466abd519c3e51b2dbcef4f9b33b0
Alina Nesterova <anesterova@mirantis.com>
2017-02-01 21:39:43 +0400

    Fixed pep and travis errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+from app import routs, events
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-
-from app import routs, events
