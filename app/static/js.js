2845d133c178d0fa50f25c902503186e6346bd10
nbelonogov <belonogovna@sgu.ru>
2017-02-01 18:22:02 +0400

    mv socketio init to jsjs

--- app/static/js.js
+++ app/static/js.js
@@ -1,3 +1,8 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function() {
+    emit('message', 'I\'m connected!');
+});
+
 function emit(event, message) {
     socket.emit(event, message);
 }
@@ -5,6 +10,5 @@ function emit(event, message) {
 function send_message(message) {
     emit('message', message)
     document.getElementById("chat_box").innerHTML += "<div><div class='message client_message'>" + message +"</div></div>";
-    // TODO: add this message to
     // TODO: clear message input box
 }
