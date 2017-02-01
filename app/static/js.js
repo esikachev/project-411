0d5931d80d3a8200711a39e33a0ec7db6d2140b8
nbelonogov <belonogovna@sgu.ru>
2017-02-01 15:07:06 +0400

    csscss

--- app/static/js.js
+++ app/static/js.js
@@ -1,3 +1,8 @@
 function emit(event, message) {
     socket.emit(event, message);
 }
+
+function send_message(object) {
+    emit('message', object.textContent)
+    // TODO: clear message input box
+}
