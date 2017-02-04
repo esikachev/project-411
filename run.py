a79a6de2727b84c859af82f21e7dcb6359d56d7e
nbelonogov <belonogovna@sgu.ru>
2017-02-04 17:15:28 +0400

    ready for CD

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
+
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
