f5e62870506d4b39a645a1a83a8dbb205a6da247
Никита Белоногов <belonogovna@sgu.ru>
2017-02-06 12:38:59 +0400

    improve  api (#24)
    * enter to send && favicon
    
    * TODO
    
    * )

--- app/events.py
+++ app/events.py
@@ -4,6 +4,12 @@ from app import socketio
 from bot import response
 
 
+@socketio.on('status')
+def handle_message(message):
+    emit('message', 'Hello.')
+    print('received status: ' + str(message))
+
+
 @socketio.on('message')
 def handle_message(message):
     emit('message', response())
