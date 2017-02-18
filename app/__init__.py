a5cc30ce02c949cdbd526168ecc88b0223e7f11d
Никита Белоногов <belonogovna@sgu.ru>
2017-02-18 13:36:09 +0400

    fix app import problem (#28)
    pep8 reconciliation

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-from app import routs, events
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+if app:
+    import routs
+    import events
