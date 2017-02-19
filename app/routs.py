0c974e3c315af15e24ab773777e21f111637dcfd
nbelonogov <belonogovna@sgu.ru>
2017-02-19 16:42:12 +0400

    add redirect

--- app/routs.py
+++ app/routs.py
@@ -1,3 +1,4 @@
+from flask import redirect
 from flask import render_template
 
 from app import app
@@ -6,3 +7,8 @@ from app import app
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect("/", code=302)
