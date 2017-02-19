55d930ecf379cbf772fb341348a680693cc93880
nbelonogov <belonogovna@sgu.ru>
2017-02-19 15:10:05 +0400

    fix indentation

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,41 +1,41 @@
 $('.chat[data-chat=person]').addClass('active-chat');
 $('.person[data-chat=person]').addClass('active');
 
-$('.left .person').mousedown(function(){
+$('.left .person').mousedown(function () {
     if ($(this).hasClass('.active')) {
         return false;
-    } 
-	else {
+    }
+    else {
         var findChat = $(this).attr('data-chat');
         var personName = $(this).find('.name').text();
-		
+
         $('.right .top .name').html(personName);
         $('.chat').removeClass('active-chat');
         $('.left .person').removeClass('active');
         $(this).addClass('active');
-        $('.chat[data-chat = '+findChat+']').addClass('active-chat');
+        $('.chat[data-chat = ' + findChat + ']').addClass('active-chat');
     }
 });
 
-function sendMessage(){
+function sendMessage() {
     var inputValue = document.getElementById('message_box').value;
 
     document.getElementById('message_box').value = '';
 
-	emit('message', inputValue);
+    emit('message', inputValue);
 
-	printMessage("me", inputValue)
+    printMessage("me", inputValue)
 }
 
 function printMessage(sender, message) {
     var newDiv = document.createElement("div");
-	var objDiv = document.getElementById("chat");
+    var objDiv = document.getElementById("chat");
 
     newDiv.innerHTML = message;
-	newDiv.className += " bubble" + " " + sender;
+    newDiv.className += " bubble" + " " + sender;
 
     objDiv.appendChild(newDiv);
-	objDiv.scrollTop = objDiv.scrollHeight;
+    objDiv.scrollTop = objDiv.scrollHeight;
 }
 
 
@@ -44,7 +44,7 @@ socket.on('connect', function () {
     emit('status', 'I\'m connected!');
 });
 
-socket.on('message', function(data){
+socket.on('message', function (data) {
     printMessage('you', data);
 });
 
